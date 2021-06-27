fb0d03aed8b190f268d10374c937ef2e546cd9ce
Softonic254 <rotichenoch27@gmail.com>
2021-06-27 14:10:39 +0300

    It adds Procfile

--- config.py
+++ config.py
@@ -40,6 +40,9 @@ class DevConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:qwerty@localhost/watchlist'
     DEBUG = True
     
